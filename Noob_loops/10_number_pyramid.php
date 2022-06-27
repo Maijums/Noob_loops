ead7e2f41fba473589eda170f496f5eac8886d60
Maija <97105846+Maijums@users.noreply.github.com>
2022-06-28 00:10:31 +0300

    Update 10_number_pyramid.php

--- Noob_loops/10_number_pyramid.php
+++ Noob_loops/10_number_pyramid.php
@@ -11,15 +11,7 @@
 1   4   6   4   1
 
  */
-/*
-$ent = readline('Enter the number: ');
-$num = $ent + ($ent - 1);
 
-for ($i = 1; $i <= $num; $i+=2) {
-    $whitespace = ($num - $i / 2);
-    echo str_pad('', $whitespace, ' ') . str_pad('', $i, '* ') . "\n";
-}
-*/
 
 $num = readline('Enter the number: (1 - 7) ');
 $max = 28;     // $num * 3 + 7;
@@ -52,15 +44,3 @@ for ($i = 1; $i <= $num; $i++) {
   echo "\n";
 }
 
-/*
-for ($i = 0; $i <= 9; $i++) {
-
-    for ($d = 10 - $i; $d > 0; $d--)  {
-        echo "1";
-    }
-    for ($j = 1; $j <= $i; $j++) {
-        echo "2" . $i . "3";
-    }
-    echo "\n";
-}
-*/
